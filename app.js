6b26f86446c5576389e9b7e451f675b3e70d9967
d3ad5h01 <iit2019220@iiita.ac.in>
2022-05-29 01:11:17 +0530

    Update servers

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-let connection = new RTCPeerConnection();
+let connection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: "stun:openrelay.metered.ca:80",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:80",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+    ],
+  });
 let localMediaStream;
 let remoteMediaStream;
 let textChannelStream;
